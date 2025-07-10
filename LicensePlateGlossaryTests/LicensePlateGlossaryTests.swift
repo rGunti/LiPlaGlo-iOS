b31bbda2a52c65eaa322b07960aaac319a5d0b69
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2025-07-10 09:05:15 +0200

    Initial Commit

---
+++ LicensePlateGlossaryTests/LicensePlateGlossaryTests.swift
@@ -0,0 +1,16 @@
+//
+//  LicensePlateGlossaryTests.swift
+//  LicensePlateGlossaryTests
+//
+//  Created by Raphael Guntersweiler on 10/07/2025.
+//
+
+import Testing
+
+struct LicensePlateGlossaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
